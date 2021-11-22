3cf6b01a72ff0074d2059a230167148bd3182b96
salamander5777 <77403008+salamander5777@users.noreply.github.com>
2021-11-22 15:22:30 -0500

    Add files via upload

---
+++ main.h
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Michael Gilday
+ */
+
+#include <iostream>
+
+//Chapter 3, Exercise #10 - (Program that takes in an operation and two operands and then outputs the resultant.)
+int main() {
+  std::string operation;
+  double a, b, math;
+  std::cout << "Input an operation: ";
+  std::cin >> operation;
+  std::cout << "Input the first operand: ";
+  std::cin >> a;
+  std::cout << "Input the second operand: ";
+  std::cin >> b;
+
+  if(operation == "+" || operation == "plus"){
+    math = a + b;
+    std::cout << "The computed value of: " << a << "+" << b << " is " << math;
+  }
+  else if(operation == "-" || operation == "minus"){
+    math = a-b;
+    std::cout << "The computed value of: " << a << "-" << b << " is " << math;
+  }
+  else if(operation == "*" || operation == "mul"){
+    math = a*b;
+    std::cout << "The computed value of: " << a << "*" << b << " is " << math;
+  }
+  else if(operation == "/" || operation == "div"){
+    math = a/b;
+    std::cout << "The computed value of: " << a << "/" << b << " is " << math;
+  }
+} 
